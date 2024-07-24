b84111ef9b6ce5f83dac7b9886892b8bf3c8f759
DBHRN <dbhrnf@gmail.com>
2024-07-23 20:39:28 -0600

    arreglando endpoint register

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "./axios";
 
-export const registerRequest = user => axios.post(`/auth/signup`, user);
+export const registerRequest = user => axios.post(`/auth/register`, user);
 
 export const loginRequest = user => axios.post(`/auth/login`, user);
 
